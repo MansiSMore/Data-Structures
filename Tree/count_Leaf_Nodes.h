28dc374db69cce5cc87aa8070031eb294e804736
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-01-14 19:44:04 +0530

    Create count_Leaf_Nodes.h

---
+++ Tree/count_Leaf_Nodes.h
@@ -0,0 +1,17 @@
+#include "binary_search_tree_header.h"
+
+//Counting all leaf nodes from a tree.
+int countLeafNodes(TNODE head)
+{
+	if(head == NULL)
+	{
+		return 0;
+	}
+	
+	if(head -> left == NULL && head -> right == NULL)
+	{
+		return 1;
+	}
+	
+	return countLeafNodes(head -> left) + countLeafNodes(head -> right);
+}
